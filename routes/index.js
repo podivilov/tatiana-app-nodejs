d0d6d473162da0f8d9c3e772f0842a9b619df0ca
Mihail Podivilov <58859358+podivilov@users.noreply.github.com>
2020-02-25 11:09:54 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
+var uuid = require('uuid');
 var express = require('express');
 var router = express.Router();
+
+router.use(function (req, res, next) {
+  // check if client sent cookie
+  var cookie = req.cookies.__tatiana_session_id;
+  if (cookie === undefined)
+  {
+    // no: set a new cookie
+    global.sessionid=uuid.v4();
+    res.cookie('__tatiana_session_id' , global.sessionid, { maxAge: 315569260000, httpOnly: true });
+    console.log('New session created:', global.sessionid);
+  }
+  else
+  {
+    // yes, cookie was already present
+    global.sessionid=cookie;
+    console.log('Attached session:', cookie);
+  }
+  next(); // <-- important!
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'botUI_api.ai' });
-  console.log(res.value);
 });
 
 
