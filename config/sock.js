4f0f6ee2ebf9bb491376a658ccda3825c5adbb69
Mihail Podivilov <58859358+podivilov@users.noreply.github.com>
2020-02-25 11:11:13 +0300

    Update sock.js

--- config/sock.js
+++ config/sock.js
@@ -7,7 +7,7 @@ var api = require('./api');
 var conn = function() {
   server.listen(8010);
 
-  app.get('/', function (req, res) {
+  app.get('/', function (req, res, next) {
     res.sendfile(__dirname + '/index.html');
   });
 };
@@ -16,13 +16,13 @@ var fromClient = function() {
 
 io.on('connection', function (socket) {
   socket.on('fromClient', function (data) {
-    console.log(data.client);
-         api.setSid(socket.id);
+    console.log(global.sessionid, data.client);
+         api.setSid(global.sessionid);
          api.getRes(data.client).then(function(res){
-           console.log('response', res);
-            socket.emit('fromServer', { server: res });
+           console.log(global.sessionid, res);
+           socket.emit('fromServer', { server: res });
          });
-  });
+    });
 });
 }
 module.exports = {conn,fromClient}
