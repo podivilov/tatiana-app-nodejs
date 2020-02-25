037fd2c13286db00e93d76e58cc5880694fa8e18
Mihail Podivilov <58859358+podivilov@users.noreply.github.com>
2020-02-25 11:10:21 +0300

    Update convo.js

--- public/javascripts/convo.js
+++ public/javascripts/convo.js
@@ -1,22 +1,34 @@
 var botui = new BotUI('api-bot');
 
-var socket = io.connect('https://tatiana-app-proxy.podivilov.ru');
+var socket = io.connect('https://ws.podivilov.ru');
 // read the BotUI docs : https://docs.botui.org/
 
 botui.message.add({
-  content: 'Привет! Меня зовут Татьяна. Я — виртуальный ассистент. Чем могу помочь?',
+  content: 'Привет!',
   delay: 1500,
 }).then(function () {
+  botui.message.add({
+  content: 'Меня зовут Татьяна и я &mdash; виртуальный ассистент &#128129;',
+  delay: 2000,
+})}).then(function () {
+  botui.message.add({
+  content: 'Я работаю в ГБПОУ МО &laquo;Колледж &laquo;Коломна&raquo; &#128188;',
+  delay: 3500,
+})}).then(function () {
+  botui.message.add({
+  content: 'С радостью отвечу на все ваши вопросы!',
+  delay: 5000,
+})}).then(function () {
   botui.action.text({
     action: {
-      placeholder: 'Скажите что-нибудь', }
+      placeholder: 'Чем я могу вам помочь?', }
   }
 ).then(function (res) {
   socket.emit('fromClient', { client : res.value }); // sends the message typed to server
-    console.log(res.value); // will print whatever was typed in the field.
+    console.log('Вы сказали:', res.value); // will print whatever was typed in the field.
   }).then(function () {
     socket.on('fromServer', function (data) { // recieveing a reply from server.
-      console.log(data.server);
+      console.log("Татьяна ответила:",data.server);
       newMessage(data.server);
       addAction();
   })
@@ -34,10 +46,10 @@ function newMessage (response) {
 function addAction () {
   botui.action.text({
     action: {
-      placeholder: 'Введите ответ...', 
+      placeholder: 'Что-нибудь ещё?',
     }
   }).then(function (res) {
     socket.emit('fromClient', { client : res.value });
-    console.log('client response: ', res.value);
+    console.log('Вы сказали:', res.value);
   })
 }
